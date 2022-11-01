8e76dfc8ab2bb53c2029b45c71cc68089dc0306d
dan-skw <danielrtfd@gmail.com>
2022-11-01 14:32:16 +0100

    problem with importing svg

--- src/components/FormsOfHelp/HelpSection.tsx
+++ src/components/FormsOfHelp/HelpSection.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Ambulance from "../../assets/ambulance.inline.svg";
 
 export const HelpSection = () => {
   return (
     <>
-      <div>siema</div>
+      <main>
+        <h1 className="text-white">Than you can help our shelter</h1>
+        <Ambulance />
+      </main>
     </>
   );
 };
